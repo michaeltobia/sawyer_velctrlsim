001bfcd74626cd70bb42bc674e5755f6a8fe560a
Michael Tobia <michaeltobia2019@u.northwestern.edu>
2018-12-08 08:31:18 -0600

    Adapting vel_ctrl and traj_gen for use with irl Sawyer

--- src/vel_ctrl.py
+++ src/vel_ctrl.py
@@ -3,21 +3,19 @@
 import rospy
 import tf
 import numpy as np
-from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState  ## Still needed?
+from std_msgs.msg import Float64, Int32
 from geometry_msgs.msg import TransformStamped
+from intera_core_msgs.msg import JointCommand
 import modern_robotics as mr
 import sawyer_MR_description as s_des
 import io_util
 
-
-# class SimpleSubEx:
-#     def __init__(self):
-#         self.traj_sub = rospy.Subscriber('desired_trajectory', TransformStamped, self.testmethod)
-#
-#     def testmethod(self, X_d):
-#         print X_d
-
+#### Intera Joint Control Modes ####
+POSITION_MODE = Int32(1)
+VELOCITY_MODE = Int32(2)
+TORQUE_MODE = Int32(3)
+TRAJECTORY_MODE = Int32(4)
 
 
 
@@ -26,36 +24,34 @@ class VelCtrl:
         ## ROS Init
         rospy.init_node('vel_ctrl')
         self.tf_lis = tf.TransformListener()
+
+        ## Trajectory Subscriber
         self.sub = rospy.Subscriber("/desired_trajectory", TransformStamped, self.ctrl_r)
 
-
         #####TESTING PUBLISHERS######
-        self.pub = rospy.Publisher('joint_states', JointState, queue_size=10) ### Rviz traj follower
+        # self.pub = rospy.Publisher('joint_states', JointState, queue_size=10) ### Rviz traj follower
 
         #### TESTESTESTESTESTESTESTESTESTESTESTESTEST ######
-        # self.pub = rospy.Publisher('robot/limb/right/joint_command', JointState, queue_size=10) ### Gazebo Simulator Only!
+        self.pub = rospy.Publisher('robot/limb/right/joint_command', JointCommand, queue_size=10) ### Gazebo Simulator Only!
         ####################################################
 
-        # self.rate = rospy.Rate(20) # 20Hz
-        ## Trajectory Subscriber
         ## Error Publishers
         # self.mean_err_pub = rospy.Publisher('/mean_error', Float64, queue_size=10)
         # self.mean_err_msg = Float64()
+
         ## Control Gains
         self.Kp = 2*np.eye(6)
         self.Ki = 1*np.eye(6)
+
         ## Robot Description
         self.B_list = s_des.Blist
         self.M = s_des.M
-        self.main_js = JointState()
-        self.main_js.name = ['right_j0', 'head_pan', 'right_j1', 'right_j2', \
+        self.joint_ctrl_msg = JointCommand()
+        self.joint_ctrl_msg.names = ['right_j0', 'head_pan', 'right_j1', 'right_j2', \
                             'right_j3', 'right_j4', 'right_j5', 'right_j6']
+        self.joint_ctrl_msg.mode = VELOCITY_MODE
+        self.joint_ctrl_msg.velocity = Float64(np.zeros(len(self.joint_ctrl_msg.names))) # 0 initial velocity
 
-        ## Joint velocity message
-        # self.main_js.velocity = np.ndarray.tolist(np.zeros(8)) #init main_js
-        ## Joint position
-        self.main_js.position = np.ndarray.tolist(np.zeros(8))
-        self.pub_main_js()
 
         self.it_count = 0 # Iteration count for debug
         self.int_err = 0 # Integrated error 0 -> t
@@ -64,15 +60,10 @@ class VelCtrl:
 
 
     def ctrl_r(self, X_d):
-        # (p_cur, Q_cur) = self.tf_lis.lookupTransform('/main_tf/base', '/main_tf/right_hand', rospy.Time(0))
-        # X_cur = self.tf_lis.fromTranslationRotation(p_cur, Q_cur)
-        # # Get cur' joint positions
-        cur_theta_list = np.delete(self.main_js.position, 1)
-        # Get EEd transform
+        # Get cur' joint positions
+        cur_theta_list = np.delete(self.joint_ctrl_msg.velocity, 1)
 
-        ####### Now pulling X_d from traj_gen
-        # ##### Change to trajectory pull!
-        # (self.p_d, self.Q_d) = self.tf_lis.lookupTransform('/ref_tf/base', '/ref_tf/right_hand', rospy.Time(0))
+        # Get EEd transform
         # ##### Need Q_d_dot (for feedforward tho)
         # # Change EEd transform to X_d
         p_d, Q_d = mr.TFtoMatrix(X_d)
@@ -80,70 +71,33 @@ class VelCtrl:
         # Get cur' X from robot_des and cur' joint positions
         X_cur = mr.FKinBody(self.M, self.B_list, cur_theta_list)
         # Get X_e from [X_e] = log(X^(-1)X_d)
-        self.X_e = mr.se3ToVec(mr.MatrixLog6(np.dot(mr.TransInv(X_cur), X_d)))
+        X_e = mr.se3ToVec(mr.MatrixLog6(np.dot(mr.TransInv(X_cur), X_d)))
         # Find integral error (with limit)
-
         ##### FIX THIS!
         if np.linalg.norm(self.int_err) < 10 and np.linalg.norm(self.int_err) > -10: # int_err limit (-10, 10)
-            self.int_err = (self.int_err + self.X_e)*(0.5/20) # Add int_err straight to X_e?
+            self.int_err = (self.int_err + X_e)*(0.5/20)
 
         # Get body twist command V_b = K_p . X_e + K_i . int(X_e)
-        self.V_b = np.dot(self.Kp, self.X_e) + np.dot(self.Ki, self.int_err)
+        V_b = np.dot(self.Kp, X_e) + np.dot(self.Ki, self.int_err)
         # Get body Jacobian from robot_des and cur' joint positions
-        self.J_b = mr.JacobianBody(self.B_list, cur_theta_list)
-        # Hybrid Control Prototype (surprise, doesnt work)
-        T_selector = np.matrix([[0,0,0],\
-                               [0,0,0],\
-                               [1,0,0],\
-                               [0,1,0],\
-                               [0,0,1],\
-                               [0,0,0]])
-        Y_selector = np.matrix([[1,0,0],\
-                               [0,1,0],\
-                               [0,0,0],\
-                               [0,0,0],\
-                               [0,0,0],\
-                               [0,0,1]])
-        # Get joint velocity command from theta_dot = pint(J_b)V_b
-        J_b_pinv = np.linalg.pinv(self.J_b)
-        theta_dot = np.dot(J_b_pinv, self.V_b)
-        coeff_null = (np.eye(7) - np.dot(self.J_b.T, J_b_pinv.T))
-        theta_dot += np.dot(coeff_null, theta_dot)
-        # theta_dot = np.dot(np.linalg.pinv(self.J_b), np.dot(self.V_b, Y_selector))
+        J_b = mr.JacobianBody(self.B_list, cur_theta_list)
 
-        # self.main_js.velocity = self.theta_dot # set new joint velocities
-        # self.main_js.header.stamp = rospy.Time.now() # get header stamp
-        # self.pub.publish(self.main_js)
+        # Get joint velocity command from theta_dot = pint(J_b)V_b
+        J_b_pinv = np.linalg.pinv(J_b)
+        theta_dot = np.dot(J_b_pinv, V_b)
 
         ##### NOT NEEDED FOR VELOCITY COMMANDS!
         # Position command stuff
         delt_theta = theta_dot*(1.0/20)
         delt_theta = np.insert(delt_theta, 1, 0)
-        self.main_js.position += delt_theta
-        self.pub_main_js()
+        self.pub_joint_ctrl_msg()
 
 
-        # ###### TESTESTESTESTESTESTESTESTESTESTESTESTEST ##############
-        # #TESTING VELOCITY COMMANDS
-        # # Position command stuff
-        # delt_theta = theta_dot
-        # delt_theta = np.insert(delt_theta, 1, 0)
-        # self.main_js.position += delt_theta
-        # self.pub_main_js()
-        # ###########################################################
 
-        # self.mean_err_msg.data = np.mean(self.X_e) ## FIX THIS
-        # print self.mean_err_msg.data ## DEBUG PRINT
-        # print np.linalg.norm(self.int_err) ## FIX THIS
-
-        # print(self.theta_dot)
-
-        # THETA DOT NEXT
-
-
-    def pub_main_js(self):
-        self.main_js.header.stamp = rospy.Time.now() # get header stamp
-        self.pub.publish(self.main_js) # publish joint velocities
+    def pub_joint_ctrl_msg(self):
+        ### Get this to produce JointCommand msgs in VelCtrl
+        self.joint_ctrl_msg.header.stamp = rospy.Time.now() # get header stamp
+        self.pub.publish(self.joint_ctrl_msg) # publish joint velocities
         # self.mean_err_pub.publish(self.mean_err_msg) # publish mean error
         # self.rate.sleep() # sleep for rate
 
@@ -152,9 +106,9 @@ class VelCtrl:
 if __name__=='__main__':
     try:
         out = VelCtrl()
-        out.pub_main_js()
+        out.pub_joint_ctrl_msg()
         rospy.spin()
         # while not rospy.is_shutdown():
-        #     out.pub_main_js()
+        #     out.pub_joint_ctrl_msg()
     except rospy.ROSInterruptException:
         raise e
